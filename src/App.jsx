e4aa9005a9310f56737da20fef7b89f280535cac
gabriela diaz <gbdadx@gmail.com>
2023-11-06 00:42:35 -0300

    async/await syntax to make an asynchronous request to the JSON file using Axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,60 @@
-import  { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import Axios from "axios";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Historial from "./pages/Historial";
+import Index from "./pages/Index";
+
+export default function App() {
+  const [data, setData] = useState([]);
+  const [datosPropiedad, setDatosPropiedad] = useState([]);
+  const [datosUbicacion, setDatosUbicacion] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await Axios.get("/src/datos.json");
+        setData(response.data);
+
+        const datosPropiedad = response.data
+          .filter((item) => item.categoria === "propiedad")
+          .map((item) => ({ tipo: item.tipo, factor: item.factor }));
+
+        const datosUbicacion = response.data
+          .filter((item) => item.categoria === "ubicacion")
+          .map((item) => ({ tipo: item.tipo, factor: item.factor }));
+
+        setDatosPropiedad(datosPropiedad);
+        setDatosUbicacion(datosUbicacion);
+      } catch (error) {
+        console.error("Error al cargar los datos:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  // You can verify the contents of 'data' here.
+  console.log("data:", data);
+  console.log("datosPropiedad:", datosPropiedad);
+  console.log("datosUbicacion:", datosUbicacion);
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index datosPropiedad={datosPropiedad} datosUbicacion={datosUbicacion} />} />
+          <Route path="/historial" element={<Historial />} />
+          <Route path="/*" element={<h1 style={{ display: 'flex', flexDirection: 'row', justifyContent:'center', alignItems:'center', paddingTop:'10rem' }}>Page not found - error 404</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+
+
+
+/*import  { useEffect, useState } from "react";
 import Axios from "axios";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Historial from "./pages/Historial"; // Importa el componente Historial
@@ -8,10 +64,9 @@ export default function App() {
   const [data, setData] = useState([]);
   const [datosPropiedad, setDatosPropiedad] = useState([]);
   const [datosUbicacion, setDatosUbicacion] = useState([]);
-  const rutaRelativa = "./src/datos.json";
 
   useEffect(() => {
-    Axios.get("./src/datos.json")
+    Axios.get("/src/datos.json")
       .then((response) => {
         setData(response.data);
 
@@ -47,4 +102,4 @@ export default function App() {
       </BrowserRouter>
     </>
   );
-}
+}*/
